23e8c759becf0e86f31f689d8d37f6ceb55aa4de
gnguyen <gnguyen5464@gmail.com>
2019-05-09 13:56:52 -0400

    Setup Devise Gem and setup authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'family_recipes#index'
 
+  devise_for :users
   resources :family_recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
